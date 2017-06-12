1785821db776014a25051acd97cb4a0a8bfe22c2
jhyun94 <hyunjonathan@gmail.com>
2017-06-12 17:01:27 -0500

    add POST todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todos');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+  var newTodo = new Todo({
+    text: req.body.text
+  });
+
+  newTodo.save().then((docs) => {
+    res.send(docs);
+  }, (err) => {
+    res.status(400).send(err);
+  })
+})
+
+app.listen(8000);
+
